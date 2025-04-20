f3c59b7fcd28f0d902dd3135e413c8eb3986b5d5
Aviweb <avi1273619@gmail.com>
2025-04-20 18:15:45 +0530

    refactored code

--- src/main/java/basic_details/demo/model/Driver.java
+++ src/main/java/basic_details/demo/model/Driver.java
@@ -1,47 +1,11 @@
-// package basic_details.demo.model;
-
-// import jakarta.persistence.*;
-
-// @Entity
-// @Table(name = "drivers")
-// public class Driver {
-
-//     @Id
-//     private String email;
-
-//     private String name;
-//     private String password;
-
-//     // Getters and Setters
-//     public String getEmail() {
-//         return email;
-//     }
-
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
-
-//     public String getName() {
-//         return name;
-//     }
-
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-
-//     public String getPassword() {
-//         return password;
-//     }
-
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-// }
-
 package basic_details.demo.model;
 
-import jakarta.persistence.*;
 import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "drivers")
